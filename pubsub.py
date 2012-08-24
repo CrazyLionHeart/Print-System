0f300a9ae345afd02994545df0023a2d0d80b677
Aleksandr Sytar <avsytar@babypages.ru>
2012-08-24 20:59:39 +0400

    Bump update

--- pubsub.py
+++ pubsub.py
@@ -42,31 +42,37 @@ class Simple(Resource):
         self.conn = cups.Connection()
 
     def _put_to_monitor(self, jobId):
+        """
+           Отправляем задание на печать в очередь мониторинга. Сообщения из очереди прилетают с задержкой в 300 секунд
+        """
+        log.info("Отправка задания печати на мониторинг")
         data = {"content": jobId, "destination": {"type": "queue", "name": "twisted_status"}, 'conf': {'AMQ_SCHEDULED_DELAY':300000, 'CamelCharsetName': 'UTF-8'} }
         self._put_to_stomp(data)
 
     def _put_to_stomp(self, data = {"content": None, "destination": {"type": None, "name": None}, "conf": {} }):
+        """
+           Отправляем сообщение в очередь используя протокол Stomp
+        """
+        log.info("Отправляем сообщение в очередь")
         stomp = Client()
         stomp.connect(username="admin", password="activemq")
         stomp.put(data['content'], destination = "/%(type)s/%(name)s" % data['destination'], conf =  data['conf'])
         stomp.disconnect()
 
-    def _get_from_stomp(self, data = {"type": "queue", "name": "queue_name"}, callbacks = None):
+    def _get_from_stomp(self, data = {"type": "queue", "name": "queue_name"}):
+        """
+           Получаем сообщение из очереди. Используем блокирующий вывод,
+           так как непонятно сколько раз щемится в очередь до получения сообщения
+        """
+        log.info("Получаем сообщение из очереди")
         stomp = Client()
         stomp.connect(username="admin", password="activemq")
         destination = "/%(type)s/%(name)s" % data
-        stomp.subscribe("/%(type)s/%(name)s" % data)
-        log.msg("Data: %s" % data)
-        if not (callbacks is None):
-            stomp.get(block=False, callback = callbacks)
-        else:
-            try:
-                message = stomp.get_nowait()
-            except stomp.Empty:
-                message = None
-            return message
-        stomp.unsubscribe("/%(type)s/%(name)s" % data)
+        stomp.subscribe(destination)
+        message = stomp.get()
+        stomp.unsubscribe(destination)
         stomp.disconnect()
+        return message.body
 
 
     def _get_preview_from_stomp(self, num_nakl, date_nakl):
@@ -75,6 +81,9 @@ class Simple(Resource):
 
     def _get_print_status(self, request):
         """ 
+           Отправляем задание на печать.
+
+        Доступные статусы:
 	IPP_JOB_ABORTED = 8
  	IPP_JOB_CANCELED = 7
  	IPP_JOB_COMPLETED = 9
@@ -86,16 +95,15 @@ class Simple(Resource):
         jobId =  request.args.get('jobId', [None])[0]
         jid = request.args.get('jid', [None])[0]
         Attributes = self.conn.getJobAttributes(int(jobId))
-        # Valid successful cups-code
+        # Определяем нужные статусы печати - которые мы не мониторим
         list = [7,9]
-        # Is job successfully printed?
+        # Если задание успешно напечаталось...
         if not find(lambda state: state == Attributes['job-state'], list):
-            # Oh, no! We just return jobId for Camel checkout
+            # Нет, задание еще висит в очереди на печать. Отправляем его в очередь мониторинга
             self._put_to_monitor(jobId)
                 
     def _print_job(self, conf = None):
         # get printer name from filename
-        log.msg("conf %s" % conf)
         printer_name = conf['printer']
         filename = urllib.url2pathname(conf['filename'])
         path = conf['path']
@@ -112,17 +120,19 @@ class Simple(Resource):
            формы.
            Возвращаемый тип документа - text/xml
         """
-        num_nakl = unicode(request.args.get('num_nakl', [None])[0], 'utf-8')
-        date_nakl = unicode(request.args.get('date_nakl', [None])[0], 'utf-8')
+
+        log.msg("Request: %s" % request.getAllHeaders())
+        log.msg("Args: %s" % request.args)
         conf = {}
-        conf["XML_GET_PARAM_num_nakl"] = num_nakl
-        conf["XML_GET_PARAM_date_nakl"] = date_nakl
+        conf["XML_GET_PARAM_num_nakl"] = request.args.get('num_nakl', [None])[0]
+        conf["XML_GET_PARAM_date_nakl"] = request.args.get('date_nakl', [None])[0]
         log.msg("JRXML conf: %s" % conf)
 
         read_data = self._get_from_stomp(data = {"type": "queue", "name": "jasper_print_data_%(XML_GET_PARAM_num_nakl)s_%(XML_GET_PARAM_date_nakl)s" % conf })
+        log.msg("Read data from stomp: %s" % read_data)
         request.setHeader('Content-Type', "text/xml")
-        if not (read_data is None):
-            return read_data
+        request.write(read_data)
+        request.finish()
 
     def _get_preview(self, request):
         """
@@ -222,6 +232,8 @@ class Simple(Resource):
                 if len(xpath):
                     conf[header] = xpath[0]
 
+#            conf['XML_URL'] = conf['XML_URL'] + '?' + 'XML_GET_PARAM_num_nakl=' + conf['XML_GET_PARAM_num_nakl'] + '&' + 'XML_GET_PARAM_date_nakl=' + conf['XML_GET_PARAM_date_nakl']
+
             """
                Разбиваем сообщение на две части - управляющую и данные
                Упаправляющую часть ...
@@ -235,7 +247,7 @@ class Simple(Resource):
             stomp_print_data = {"content": etree.tostring(print_data[0], encoding='utf-8', pretty_print=True), "destination": {"type": "queue", "name": "jasper_print_data_%(XML_GET_PARAM_num_nakl)s_%(XML_GET_PARAM_date_nakl)s" % conf}, "conf": conf }
             stomp_control_control_data = {"content": etree.tostring(control_data[0], encoding='utf-8', pretty_print=True), "destination": {"type": "queue", "name": "jasper_jasper_control"}, "conf": conf }
             stomp_control_data = {"content": etree.tostring(control_data[0], encoding='utf-8', pretty_print=True), "destination": {"type": "queue", "name": "jasper_control_data"}, "conf": conf }
-
+            
             d1 = deferLater(reactor, 0, lambda: stomp_print_data)
             d1.addCallback(self._put_to_stomp)
             d1.addErrback(log.err)
@@ -249,6 +261,10 @@ class Simple(Resource):
             d2.addErrback(log.err)
 
             return "XML parsed"            
+        elif (self.uri == "test"):
+            log.msg("Headers: %s" % request.getAllHeaders())
+            log.msg("Body: %s" % request.content.read())
+            return "QE{"
         else:
             return "OK"
 
